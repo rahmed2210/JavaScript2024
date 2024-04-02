dce5b54483af85b8f38b61dc5e76337f848ec517
Rubal <rahmed2210@gmail.com>
2024-04-02 08:16:50 -0400

    Variables Numbers

---
+++ Variables/02_Numbers.js
@@ -0,0 +1,28 @@
+console.log('\r\n..................((((( Exercise One ))))).......................\r\n')
+
+let num = 34 + 2
+
+console.log(num)
+
+let x = 11
+let num1 = x + 1 * 2
+console.log(num1)
+
+let age = 42
+let dogYears = (age + 1) / 7
+console.log(dogYears)
+
+
+
+//-----------------------  CHALLENGE   ---------------------------------------------------
+console.log('\r\n..................((((( Challenge Area ))))).......................\r\n')
+
+// studentScore  18
+// maxScore 20
+// percent 90
+
+let studentScore = 18
+let maxScore = 20
+let percent = (studentScore / maxScore) * 100
+
+console.log(percent)
