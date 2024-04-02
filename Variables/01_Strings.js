08eede50ad969987b19d1ffdbb33c32ea19dff2a
Rubal <rahmed2210@gmail.com>
2024-04-02 07:30:25 -0400

    Variable Strings

---
+++ Variables/01_Strings.js
@@ -0,0 +1,31 @@
+// let  --> know as a reserved keyword in JS, when JS runs through files and LET, 
+//          its going to know that we trying to create a variables.
+
+console.log('\r\n..................((((( Exercise One ))))).......................\r\n')
+let name = 'Rubal';
+
+console.log(name);
+
+
+console.log('\r\n..................((((( Exercise Two ))))).......................\r\n')
+let firstName = 'Rubal';
+let lastName = 'Ahmed';
+let fullName = firstName + ' ' + lastName;
+//   + ' ' +  --> string for space between first and last name
+
+console.log(fullName);
+
+
+//-----------------------  CHALLENGE   ---------------------------------------------------
+console.log('\r\n..................((((( Challenge Area ))))).......................\r\n')
+
+// Use variables, strings and string concatenation to solve the problem
+//   -- String concatenation --> is the operation of joining character strings end-to-end
+
+// City, Country, Location
+
+let city = 'Paterson'
+let Country = 'United State'
+let Location = city + ', ' + Country
+
+console.log(Location)
