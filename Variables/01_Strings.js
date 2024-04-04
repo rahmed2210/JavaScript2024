6df7dabfdb1bd6ba4926aac389befece7c1482a5
Rubal <rahmed2210@gmail.com>
2024-04-03 20:45:24 -0400

    Strings Variables

--- Variables/01_Strings.js
+++ Variables/01_Strings.js
@@ -1,25 +1,25 @@
 // let  --> know as a reserved keyword in JS, when JS runs through files and LET, 
 //          its going to know that we trying to create a variables.
 
-console.log('\r\n..................((((( Exercise One ))))).......................\r\n')
+console.log('\r\n................((((( Exercise One ))))).......................\r\n')
 let name = 'Rubal';
 
 console.log(name);
 
 
-console.log('\r\n..................((((( Exercise Two ))))).......................\r\n')
+console.log('\r\n..................((((( Exercise Two )))))......................\r\n')
 let firstName = 'Rubal';
 let lastName = 'Ahmed';
 let fullName = firstName + ' ' + lastName;
-//   + ' ' +  --> string for space between first and last name
+//   + ' ' +  -->   string for space between first and last name
 
 console.log(fullName);
 
 
-//-----------------------  CHALLENGE   ---------------------------------------------------
-console.log('\r\n..................((((( Challenge Area ))))).......................\r\n')
+//-----------------------  CHALLENGE   -------------------------------------------------
+console.log('\r\n.................((((( Challenge Area )))))......................\r\n')
 
-// Use variables, strings and string concatenation to solve the problem
+//   Use variables, strings and string concatenation to solve the problem
 //   -- String concatenation --> is the operation of joining character strings end-to-end
 
 // City, Country, Location
