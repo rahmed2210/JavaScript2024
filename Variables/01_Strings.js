e7075ad80214c7c8ddbd4b67ac4b72ca76e7288d
Rubal <rahmed2210@gmail.com>
2024-04-05 21:32:06 -0400

    Starings variables

--- Variables/01_Strings.js
+++ Variables/01_Strings.js
@@ -1,13 +1,14 @@
-// let  --> know as a reserved keyword in JS, when JS runs through files and LET, 
-//          its going to know that we trying to create a variables.
+// let  --> know as a reserved keyword in JS, 
+//           when JS runs through files and LET, 
+//           its going to know that we trying to create a variables.
 
-console.log('\r\n................((((( Exercise One ))))).......................\r\n')
+console.log('\r\n................((((( Exercise One ))))).......................')
 let name = 'Rubal';
 
 console.log(name);
 
 
-console.log('\r\n..................((((( Exercise Two )))))......................\r\n')
+console.log('\r\n..................((((( Exercise Two )))))......................')
 let firstName = 'Rubal';
 let lastName = 'Ahmed';
 let fullName = firstName + ' ' + lastName;
@@ -16,8 +17,8 @@ let fullName = firstName + ' ' + lastName;
 console.log(fullName);
 
 
-//-----------------------  CHALLENGE   -------------------------------------------------
-console.log('\r\n.................((((( Challenge Area )))))......................\r\n')
+//------------------------------  CHALLENGE   ----------------------------------
+console.log('\r\n.................((((( Challenge Area )))))......................')
 
 //   Use variables, strings and string concatenation to solve the problem
 //   -- String concatenation --> is the operation of joining character strings end-to-end
@@ -35,4 +36,4 @@ let person = 'Rubal'
 let age = 42
 let male = 'My name is ' + person + ', ' + ' i am ' + age + ' years old'
 
-console.log(male)
+console.log(male + '\r\n')
