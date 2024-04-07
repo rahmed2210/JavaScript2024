36340e9e47c526f8c399223dfa04d782bd527065
Rubal <rahmed2210@gmail.com>
2024-04-07 18:31:18 -0400

    Variables

---
+++ Variables/03_variables.js
@@ -0,0 +1,11 @@
+// 1  --> You can't define a variable more than once
+
+let petName = 'Belle'
+petName = 'Spot'
+
+// can't use let twice to define a variable twice, it's okay to reassign a variable's value
+
+// 2  -->  There are rules related to variable names 
+
+// 3  -->  Variable names cannot be reserved keywords
+//  let let == 12
