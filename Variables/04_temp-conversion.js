95c1aa8f5cefde690f92dba9d0331510d83f7095
Rubal <rahmed2210@gmail.com>
2024-04-07 23:30:29 -0400

    Temp Coversion

---
+++ Variables/04_temp-conversion.js
@@ -0,0 +1,15 @@
+let  fahrenheit = 32  // 0c - 273.15k
+//  T(°F) = T(°C) × 9/5 + 32
+
+let celsius = (fahrenheit - 32) * 5 / 9
+
+// T(K) = (T(°F) + 459.67)× 5/9
+let kelvin = (fahrenheit + 459.67) * 5 / 9
+
+console.log("Celsius = " + celsius)
+
+console.log("Kelvin = " + kelvin)
+
+
+
+
