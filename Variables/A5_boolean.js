eb01dce57f3556a60ee504e9f0c18b3d1f2b00b6
Rubal <rahmed2210@gmail.com>
2024-04-17 22:31:40 -0400

    Boolean

--- Variables/A5_boolean.js
+++ Variables/A5_boolean.js
@@ -4,28 +4,34 @@ let temp = 31
 let isFreezing = temp === 31
 console.log('Equality Operator:-->  ' + isFreezing)
 
+
 // Not Equal Operator:  !==
 let isFreezing1 = temp !== 32
 console.log('Not Equal:-->  ' + isFreezing1)
+
 
 // Less Than Operator:  <
 let isFreezing2 = temp < 32
 console.log('Less Than:-->  ' + isFreezing2)
 
+
 // Greater Than Operator:  >
 let isFreezing3 = temp > 32
 console.log('Greater Than:-->  ' + isFreezing3)
 
+
 // Greater Than OR Equal to Operator:  >=
 let isFreezing4 = temp >= 32
 console.log('Greater Than OR Equal to:-->  ' + isFreezing4)
+
 
 // Less Than Equal to Operator:  >=
 let isFreezing5 = temp <= 32
 console.log('Less Than OR Equal to:-->  ' + isFreezing5)
 
 
-//  ----------------- CHALLENGE AREA --------------------
+//  ----------------------- CHALLENGE AREA --------------------------
+//  -----------------------------------------------------------------
 
 // create age set to your age
 // calculate is child = if they are 7 or under
@@ -33,9 +39,11 @@ console.log('Less Than OR Equal to:-->  ' + isFreezing5)
 // print is child value
 // print is senior value
 
+
 let age = 42
 let isChild = age <= 7
 let isSenior = age >= 65
+
 
 console.log('isChild:-->  ' + isChild)
 console.log('isSenior:-->  ' + isSenior)
@@ -44,6 +52,7 @@ console.log('isSenior:-->  ' + isSenior)
 // > - greater than Operator
 // <= - less than or equal to operator
 // >= - greater than or equal to Operator
+
 
 let exTemp = 180
 
@@ -54,6 +63,7 @@ if (exTemp  <= 32) {
 if (exTemp  >= 110) {
     console.log('It is way to hot outside!')
 }
+
 
 // Challenge area
 
