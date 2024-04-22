8da471c9b75f4afdfd244fd79f83cce042deff64
Rubal <rahmed2210@gmail.com>
2024-04-22 16:35:01 -0400

    String Methods

--- Objects/c6_string-methods.js
+++ Objects/c6_string-methods.js
@@ -31,3 +31,33 @@ console.log(password.includes('password'))
 console.log(name.trim())
 
 
+
+
+//================== CHALLENGE AREA ===================================
+console.log('\r\n <==== CHALLENGE AREA ====> \r\n')
+
+
+// isValidPassword
+let isValidPassword = function (password) {
+    if (password.length > 8 && password.includes('password')) {
+        return true
+    } else {
+        return false
+    }
+}
+// Lenght is more than 8 - and it doesn't contain the word password
+
+console.log(isValidPassword('asdfp'))
+console.log(isValidPassword('abc123!@#$%^&'))
+console.log(isValidPassword('asdfpasdfpoijpassword'))
+
+/* Simplify it
+in a situation where you have an if statement like this ^^, if condition return boolean else return the other boolean
+*/ 
+let isValidPasswordOne = function (passwordOne) {
+    return passwordOne.length > 8 && passwordOne.includes('password')
+}
+
+console.log(isValidPasswordOne('asdfp'))
+console.log(isValidPasswordOne('abc123!@#$%^&'))
+console.log(isValidPasswordOne('asdfpasdfpoijpassword'))
