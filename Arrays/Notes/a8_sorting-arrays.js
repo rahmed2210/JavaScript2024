231a39ec322e4eccb62d7a3d11405e96985fca70
Rubal <rahmed2210@gmail.com>
2024-04-25 19:15:56 -0400

    Sorting Arrays

---
+++ Arrays/Notes/a8_sorting-arrays.js
@@ -0,0 +1,63 @@
+const notes = [{
+    title: 'my next trip',
+    body: 'i would like to go to spain'
+  }, {
+    title: 'habbits to work on',
+    body: 'exercise. eating a bit better'
+  }, {
+    title: 'office modification',
+    body: 'get a new seat'
+  }]
+  
+  // console.log(notes.pop())
+  // notes.push('My new note')
+  
+  // console.log(notes.shift())
+  // notes.unshift('My first note')
+  
+  // notes.splice(1, 1, 'This is the new second item')
+  
+  // notes[2] = 'this is now the new note 3'
+  
+  // notes.forEach(function (item, index) {
+  //   console.log(index)
+  //   console.log(item)
+  // })
+  
+  console.log(notes.length)
+  console.log(notes)
+  
+  
+  //  ---<> Searching Arrays Part 1  <>---
+  
+  // ---> indexOf -- to find if a note exists or maybe locate to replace or delete it
+  console.log()
+  
+  /*  when comparing two objects, it does not matter if they have the same
+  set of objects properties and the same object property values that does
+  not make two object equal  */
+  
+  // what make two object equal is if they're the exact same object in memory
+  
+  // let somObject ={}
+  // let otherObject = somObject
+  // console.log(somObject === otherObject)
+  
+  // alternative way to get the index of something in array, knowing that something is an object
+  
+  
+  // --> findIndex
+  console.log('----------------------------')
+  notes.findIndex(function (note, index) {
+    console.log(note)
+  })
+  
+  console.log('----------------------------')
+  const index = notes.findIndex(function (note, index) {
+    console.log(note)
+    return note.title === 'habbits to work on'
+  })
+  console.log(index)
+  
+  // findIndex --> do is return either true or false
+  
