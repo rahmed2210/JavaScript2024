7ebd480a87955517b567cf1adcf868f802e91761
Rubal <rahmed2210@gmail.com>
2024-04-28 11:56:36 -0400

    how to add new element to html from js files

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,8 +1,22 @@
+/*
 // DOM --> Document Object Model
 
 // Query and remove
 
 // http://127.0.0.1:5500/notes-app/index.html
+*/
+
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise. Eating a bit better.'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
+
 
 
 const removeOne =document.querySelector('p')  // select the first paragraph tag
@@ -12,7 +26,14 @@ const removeOne =document.querySelector('p')  // select the first paragraph tag
 const removeAll = document.querySelectorAll('p') // query selector all to find all paragraph tags
 removeAll.forEach(function (p) {
     p.textContent = '******'  //censor all the content on the screen
-   console.log(p.textContent) // property to read the value
+//   console.log(p.textContent) // property to read the value
 //    p.remove()
 })
 
+// add new element ---> 
+/*        create element allows us to create a new element, is not actually going to put it anywhere 
+          in the visible HTML, it just going to give us a reference 
+*/
+const newParagraph = document.createElement('p')
+newParagraph.textContent = 'This is a new element from JavaScript'
+document.querySelector('body').appendChild(newParagraph)
