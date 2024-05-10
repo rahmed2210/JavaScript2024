0c2d9a4ec61b1f10cb7a2200b28fa170924b27ec
Rubal <rahmed2210@gmail.com>
2024-05-09 22:49:56 -0400

    refactors

--- todo-App/todo-app.js
+++ todo-App/todo-app.js
@@ -1,5 +1,4 @@
-let todos = []
-
+const todos = getSavedTodos()
 
 // Create Filter
 const filters = {
@@ -7,50 +6,13 @@ const filters = {
     hideCompleted: false
 }
 
-
-const todosJSON = localStorage.getItem('todos')
-
-if (todosJSON !== null) {
-    todos = JSON.parse(todosJSON)
-}
-
-// Render Todos
-const renderTodos = function (todos, filters) {
-    let filteredTodos = todos.filter(function (todo) {
-        // avoid two filters call altogether
-        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
-        const hideCompletedMatch = !filters.hideCompleted ||!todo.completed
-
-        return searchTextMatch && hideCompletedMatch
-    })
-
-    const incompleteTodos = filteredTodos.filter(function (todo) {
-        return !todo.completed
-    })
-
-    document.querySelector('#todos').innerHTML = ''
-
-    const summary = document.createElement('h2')
-    summary.textContent = `You have ${incompleteTodos.length} todos left`
-    document.querySelector('#todos').appendChild(summary)
-
-    filteredTodos.forEach(function (todo) {
-        const p = document.createElement('p')
-        p.textContent = todo.text
-        document.querySelector('#todos').appendChild(p)
-    })
-}
-
-
 renderTodos(todos, filters)
-
 
 // setup event listener
 document.querySelector('#search-text').addEventListener('input', function (e) {
     filters.searchText = e.target.value
     renderTodos(todos, filters)
 })
-
 
 //  setup an submit handler and cancel the default action
 document.querySelector('#new-todo').addEventListener('submit', function (e) {
@@ -60,7 +22,7 @@ document.querySelector('#new-todo').addEventListener('submit', function (e) {
         text: e.target.elements.text.value,
         completed: false
     })
-    localStorage.setItem('todos', JSON.stringify(todos))
+    saveTodos(todos)
     //  Rerender the application
     renderTodos(todos, filters) 
     //  clear input field value
@@ -71,3 +33,5 @@ document.querySelector('#hide-completed').addEventListener('change', function (e
     filters.hideCompleted = e.target.checked
     renderTodos(todos, filters)
 })
+
+
