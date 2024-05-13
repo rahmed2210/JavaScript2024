ab8040d763438f504d0effcd7afbcbdec8ca0319
Rubal <rahmed2210@gmail.com>
2024-05-13 06:36:52 -0400

    Rendering DOM

--- todo-App/todo-functions.js
+++ todo-App/todo-functions.js
@@ -25,7 +25,7 @@ const renderTodos = function (todos, filters) {
         
         return searchTextMatch && hideCompletedMatch
     })
-
+    
     const incompleteTodos = filteredTodos.filter(function (todo) {
         return !todo.complete
     })
@@ -39,11 +39,32 @@ const renderTodos = function (todos, filters) {
 }
 
 
+//   1.     Setup a root div
+//   2.     setup and append a checkbox (set type attribute)
+//  someNode.setAttribute('type', 'checkbox')
+//   3.     setup and append a span (set text)
+//   4.     setup and append a button (set text)
+
 // Get the DOM elements for an individual note
 const generateTodoDOM = function (todo) {
-    const p = document.createElement('p')
-    p.textContent = todo.text
-    return p
+    const todoEl = document.createElement('div') //  setup a root div
+    const checkbox = document.createElement('input') // setup and append a checkbox (set type attribute
+    const todoText = document.createElement('span')  //  setup and append a span (set text)
+    const removeButton = document.createElement('button') //  setup and append a button (set text)
+
+    // Setup todo checkbox 
+    checkbox.setAttribute('type', 'checkbox')
+    todoEl.appendChild(checkbox)
+
+    // Setup the todo text  
+    todoText.textContent = todo.text
+    todoEl.appendChild(todoText)
+
+    // Setup the remove button
+    removeButton.textContent = 'x'
+    todoEl.appendChild(removeButton)
+
+    return todoEl
 }
 
 
